749a4577c5b3f411fcf0871c15b59a356aad61d6
guestxy <mr.1657.1605@gmail.com>
2025-09-27 10:15:44 +0700

    Final fix: Ensure correct project structure and restart TS server

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https', // Dùng https cho production
-        // Sử dụng biến môi trường thay vì hardcode
-        // Điều này giúp config chạy được cả ở local và production
         hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || '127.0.0.1',
-        port: process.env.NEXT_PUBLIC_API_HOSTNAME ? '' : '8000', // Chỉ dùng port 8000 khi ở local
+        port: process.env.NEXT_PUBLIC_API_HOSTNAME ? '' : '8000',
         pathname: '/media/**',
       },
     ],
   },
-  // Bạn có thể thêm các cấu hình khác của Next.js vào đây nếu cần
 };
 
-// Sử dụng 'export default' thay vì 'module.exports'
 export default nextConfig;
